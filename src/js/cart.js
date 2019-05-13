34ea519aaff64912c4da1534852462f33f324963
HElikes <545041886@qq.com>
2019-05-13 21:24:01 +0800

    "chore:代码重构“

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,6 +1,69 @@
 require(["config"],() => {
-    require(["header","footer"],() => {
-        console.log(header);
-        console.log(footer);
+    require(["url","template","header","footer"],(url,template) => {
+        //console.log(header);
+        //console.log(footer);
+        class Cart{
+            constructor(){
+                // this.shopList = $(".shop-list");
+                // this.delete = $(".delete");
+                // this.aCheck = $(".allCheck");
+                // this.check = $(".check");
+                // this.rNum = $(".reduceNum");
+                // this.aNum = $(".addNum");
+                // this.inputNum = $(".inputNum");
+                // this.subtotal = $(".subtotal");
+                // this.cleanCart = $(".cleanCart");
+                // this.count = $(".count");
+                // this.money = $(".money");
+                // this.settlement = $(".settlement");
+                //console.log(this.shopList);
+                //this.getType();
+                this.init();
+                this.bindEvents();
+            }
+            //获取分类数据
+            // getType () {
+            //     //ajax请求数据
+            //     $.get( url.rapBaseUrl + 'cart/get',data =>{
+            //         if(data.res_code === 1){
+            //             //console.log(data.res_body.data);
+            //             this.renderType(data.res_body.data);
+            //             // let list = data.res_body.list;
+            //             // let html = template("list-monopoly",{list});
+            //             // $("#shopList").html(html);
+            //         }
+            //     })
+            // }
+            // renderType (data) {
+            //     let html = template("detail-template",{data});
+            //     $("#shop-list").html(html);
+            // }
+            init(){
+                let cart = localStorage.getItem('cart');
+                //console.log(cart);
+                if(cart){
+                    //购物车有商品，对其进行渲染
+                    cart = JSON.parse(cart);
+                    this.render(cart);
+                }else{
+                    //购物车为空
+                    console.log($("#no-shop"));
+                    $("#no-shop").show();
+                    $("#left").hide();
+                }
+            }
+            render(cart){
+                let html = template("detail-template",{data: cart});
+                $("#shop-list").html(html);
+            }
+            bindEvents(){
+                //给删除按钮绑事件
+                // this.delete.onclick = () =>{
+                //     let li = this.delete.parentNode.parentNode;
+                //     console.log(li);
+                // }
+            }
+        }
+        new Cart();
     })
 })
--- src/js/detail.js
+++ src/js/detail.js
@@ -5,6 +5,7 @@ require(["config"],() => {
         class Detail {
             constructor () {
                 this.init();
+                this.addCart();
                 //this.zoom();
             }
             init(){
@@ -19,6 +20,8 @@ require(["config"],() => {
                         //当为真是接口时，不需要下面这句代码
                         data = {...data,id};
                         //data.id = id;
+                        //将data存为全局的变量
+                        this.data = data;
                         this.render(data);
                     }
                 })
@@ -28,6 +31,42 @@ require(["config"],() => {
                 let html = template('shop-detail',{data});
                 $("#shopDetail").html(html);
                 this.zoom();
+            }
+            //增加购物车功能
+            addCart(){
+                //按钮可能是以后数据渲染的，所以将事件绑给父级
+                $("#shopDetail").on('click','#btn',() => {
+                    //列表页用自定义属性，详情页不用
+                    //let id = $(this).attr(data.id);
+                    //取到这条id对应的数据
+                    //把this.data存到localstorage里
+                    //先将localstorage中的cart取出来
+                    let cart = localStorage.getItem("cart");
+                    //判断localstorage中是否有数据
+                    if(cart){
+                        //已经存过购物车了。先将cart转为字符串
+                        //再判断是否有存过当前商品
+                        cart = JSON.parse(cart);
+                        let index = -1;
+                        //some方法只要找到满足条件的就停止了
+                        if(cart.some((item,i) => {
+                            //这里index的值就是满足条件的这条商品
+                            index = i;
+                            return item.id === this.data.id;
+                        })){
+                            //代表有这条商品
+                            cart[index].num++;
+                        }else{
+                            //代表没有这条商品
+                            cart.push({...this.data,num:1});
+                        }
+                        //console.log(cart);
+                    }else{
+                        //代表购物车为空,且默认第一次只买一个
+                        cart = [{...this.data,num : 1}];
+                    }
+                    localStorage.setItem('cart',JSON.stringify(cart));
+                })
             }
             zoom () {
                 // 放大镜插件
